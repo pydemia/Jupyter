0da09b80a6cdcdf91884f145455344a99acf17c0
Young Ju Kim <pydemia@gmail.com>
2018-06-28 01:25:14 +0900

    Create jupyter_notebook_config.py

---
+++ scripts/jupyter_notebook_config.py
@@ -0,0 +1,7 @@
+c.NotebookApp.ip = '0.0.0.0'
+c.NotebookApp.open_browser = False
+c.NotebookApp.password = 'sha1:7aed0d31'
+c.NotebookApp.port = 8989
+c.NotebookApp.token = "sha1:aaad7f462634f80a998"
+c.NotebookApp.websocket_url = 'ws://ubuntu.pydemia.org:8989'
+
